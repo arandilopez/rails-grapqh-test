1f4c7dc9ab04ec9dc0f91ca1d100cbc0f9daeb75
Arandi Lopez <arandilopez.93@gmail.com>
2021-07-13 14:34:41 -0500

    add test for the complex query

--- test/integration/companies_query_test.rb
+++ test/integration/companies_query_test.rb
@@ -38,7 +38,47 @@ class CompaniesQueryTest < ActionDispatch::IntegrationTest
       post graphql_url, params: { query: query }
     end
 
-    # Expect 4 queries, one for each related model: Company, Department, Employee, Role
-    assert_equal recorder.count, 4
+    # Expect 4 queries, one for each related model: Company -> Department -> Employee -> Role
+    assert_equal 4, recorder.count
+  end
+
+  # This is a more complex query as Roles are not directly related to Companies.
+  # To fetch the roles by company I perform a complex query that joins employees, departemnts and company
+  # to filter the roles by company.
+  # Same with employees, If we fetch employees from roles without filtering the roles on the company
+  # we cannot get que subset of related employees to the role on the respective company.
+  # BatchLoader supports doing these complex queries too.
+  test 'number of queries performed on db are exactly 4 for a complex nested query' do
+    query = <<-GRAPHQL
+    {
+      companies {
+        nodes {
+          id
+          name
+          roles {
+            nodes {
+              name
+              employees {
+                nodes {
+                  name
+                  email
+                  department {
+                    name
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+    GRAPHQL
+
+    recorder = ActiveRecord::QueryRecorder.new(skip_cached: false) do
+      post graphql_url, params: { query: query }
+    end
+
+    # Expect 4 queries, one for each related model: Company -> Role -> Employee -> Department
+    assert_equal 4, recorder.count
   end
 end
